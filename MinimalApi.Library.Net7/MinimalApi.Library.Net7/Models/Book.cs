bd3facf04101ce84bc2943ebbc51b82608991d54
Lucas R Sena <msbowelite@gmail.com>
2023-06-25 12:16:25 -0300

    To dont miss code

--- MinimalApi.Library.Net7/MinimalApi.Library.Net7/Models/Book.cs
+++ MinimalApi.Library.Net7/MinimalApi.Library.Net7/Models/Book.cs
@@ -3,15 +3,29 @@
     public class Book
     {
         public string Isbn { get; set; } = default!;
-
         public string Title { get; set; } = default!;
-
         public string Author { get; set; } = default!;
-
         public string ShortDescription { get; set; } = default!;
-
-        public int PageCount { get; set; }
-
+        public PackedValue PageCount { get; set; }
         public DateTime ReleaseDate { get; set; }
+    }
+
+    public struct PackedValue
+    {
+        private PackedValue(int val)
+        {
+            if (val >= (1 << 12)) throw new ArgumentOutOfRangeException("val");
+            this._value = val;
+        }
+        private int _value;
+        public static explicit operator PackedValue(int value)
+        {
+            return new PackedValue(value);
+        }
+
+        public static implicit operator int(PackedValue me)
+        {
+            return me._value;
+        }
     }
 }
